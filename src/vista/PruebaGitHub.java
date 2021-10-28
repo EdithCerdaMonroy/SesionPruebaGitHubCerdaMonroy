f7ab55c77dbffd5156a3a17458f3c7f74959c62c
EdithCerdaMonroy <edith@LAPTOP-OC8DGAUU>
2021-10-28 18:22:09 -0500

    Prueba 1

---
+++ src/vista/PruebaGitHub.java
@@ -0,0 +1,15 @@
+
+package vista;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author edith
+ */
+public class PruebaGitHub {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Prueba GitHub\nCerdaMonroy\nIngenieria de Software");
+    }
+    
+}
